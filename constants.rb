bc2f2ea99cf53f8c7194033aea1198385e32ce33
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2021-11-18 23:36:32 +0900

    update

--- constants.rb
+++ constants.rb
@@ -6,20 +6,21 @@ URLS_PATH = 'urls.txt'
 
 MBTILES_PATH = 'tiles.mbtiles'
 MINZOOM = 8
-MAXZOOM = 17
+MAXZOOM = 16
 
 BASE_URL = 'https://x.optgeo.org/a-1/zxy'
 
 DESIGN = YAML.load <<-EOS
 -
   minzoom: 11
-  maxzoom: 17
+  maxzoom: #{MAXZOOM}
   glob: #{SRC_DIR}/vector_shp_2500**/*
 EOS
 
 Z_CONFIG = YAML.load <<-EOS
 91: 
   layer: contour
+  minzoom: 15
 100:
   layer: symbol
 2102:
@@ -35,8 +36,10 @@ Z_CONFIG = YAML.load <<-EOS
   layer: line
 5101:
   layer: building
+  minzoom: 13
 5111:
   layer: building
+  minzoom: 13
 6101:
   layer: symbol
 6111:
@@ -57,16 +60,22 @@ Z_CONFIG = YAML.load <<-EOS
   minzoom: 10
 8211:
   layer: contour
+  minzoom: 14
 8214:
   layer: contour
+  minzoom: 14
 8221:
   layer: contour
+  minzoom: 15
 8222:
   layer: contour
+  minzoom: 15
 8224:
   layer: contour
+  minzoom: 15
 8811:
   layer: contour
+  minzoom: 14
 9201:
   layer: contour
   minzoom: 10
@@ -75,18 +84,25 @@ Z_CONFIG = YAML.load <<-EOS
   minzoom: 10
 9211:
   layer: contour
+  minzoom: 14
 9212:
   layer: contour
+  minzoom: 14
 9214:
   layer: contour
+  minzoom: 14
 9221:
   layer: contour
+  minzoom: 15
 9224:
   layer: contour
+  minzoom: 15
 9501:
   layer: area
+  minzoom: 13
 9601:
   layer: area
+  minzoom: 13
 EOS
 
 JOBS = 1
